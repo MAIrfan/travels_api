f832fdd00c12087d668507b0dcad13958c1c4c19
MAIrfan <mohammedabdulirfan526@gmail.com>
2023-02-13 11:10:25 +0530

    routes fixed

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const { Driver, Invoice } = require("./model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ msg: "server up and running!" });
+  res.status(500).json({ msg: "server up and running!" });
 });
 
 router.post("/drivers", (req, res) => {
@@ -16,46 +16,45 @@ router.post("/drivers", (req, res) => {
     });
     driver
       .save()
-      .then(() => res.json("Saved new driver"))
+      .then(() => res.status(200).json(driver))
       .catch((error) => res.json(error));
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 });
 
 router.get("/drivers", async (req, res) => {
   try {
     const drivers = await Driver.find();
-    res.json(drivers);
+    res.status(200).json(drivers);
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 });
 
 router.post("/invoices", async (req, res) => {
   try {
-    const invoice = new Invoice({
+    let invoice = await Invoice.create({
       name: req.body.name,
       mobile: req.body.mobile,
       pickup: req.body.pickup,
       drop: req.body.drop,
       driver: req.body.driver,
-    }).populate('driver');
-    invoice
-      .save()
-      .then(() => res.json(invoice))
-      .catch((error) => res.json(error));
+    });
+    invoice = await invoice.populate('driver');
+
+    res.status(200).json(invoice);
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 });
 
 router.get("/invoices", async (req, res) => {
   try {
-    const invoices = await Invoice.find().populate('driver');
-    res.json(invoices);
+    const invoices = await Invoice.find().populate("driver");
+    res.status(200).json(invoices);
   } catch (error) {
-    res.json(error);
+    res.status(500).json(error);
   }
 });
 
